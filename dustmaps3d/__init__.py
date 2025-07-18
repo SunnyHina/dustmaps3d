6df3b3cb91ead3fa52f9385cc094644de0248e2d
Grapeknight <wt@mail.bnu.edu.cn>
2025-07-18 21:58:26 +0800

    🔄 Update version, docs, and data link

--- dustmaps3d/__init__.py
+++ dustmaps3d/__init__.py
@@ -1,4 +1,4 @@
-from .core import dustmaps3d, plot_dust_xyz
+from .core import dustmaps3d
 
-__version__ = '2.1.15'
-__all__ = ['dustmaps3d', 'plot_dust_xyz']
+__version__ = '2.1.16'
+__all__ = ['dustmaps3d']
